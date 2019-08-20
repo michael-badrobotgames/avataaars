6e96ca6f3ffa0bb4cdc7cbb2a845c514fce233cc
Michael Highland <michael.highland.contractor@badrobotgames.com>
2019-08-20 13:29:46 -0700

    Adding open mouth

---
+++ src/avatar/face/mouth/Open.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+
+export default class Open extends React.Component {
+  static optionValue = 'Open'
+
+  render () {
+    return (
+      <g
+        id='Mouth/Open'
+        transform='translate(2.000000, 52.000000)'
+        fillOpacity='0.699999988'
+        fill='#000000'>
+        <path
+          d="M38,15.3292014 C38,24.1657574 45.163444,31.3292014 54,31.3292014 C62.836556,31.3292014 70,24.1657574 70,15.3292014 C70,9.11045137 64.6666667,6.00107637 54,6.00107637 C43.3333333,5.93355337 38,9.04292837 38,15.3292014 Z"
+          id='Mouth'
+          transform='translate(54.005357, 22.188409) scale(1, -1) translate(-54.005357, -22.188409) '
+        />
+      </g>
+    )
+  }
+}
